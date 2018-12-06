7db324b51b897c6cfefb640bb67475be1265b8e3
Yuko Zheng <1@1234.sh>
2018-12-06 13:59:55 +0800

    Adding publish profiles

--- Andoromeda.Kyubey.Timers/Startup.cs
+++ Andoromeda.Kyubey.Timers/Startup.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Andoromeda.Kyubey.Models;
 
 namespace Andoromeda.Kyubey.Timers
 {
@@ -13,21 +11,28 @@ namespace Andoromeda.Kyubey.Timers
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddConfiguration(out var Config);
             services.AddEosNodeApiInvoker();
+            services.AddTimedJob();
+            services.AddEntityFrameworkMySql()
+                .AddDbContext<KyubeyContext>(x =>
+                {
+                    x.UseMySql(Config["MySQL"]);
+                });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Kyubey Timers are running...");
             });
+            
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<KyubeyContext>().Database.EnsureCreated();
+                app.UseTimedJob();
+            }
         }
     }
 }
