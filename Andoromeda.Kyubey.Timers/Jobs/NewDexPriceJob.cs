e32f2773087b2407bc729cba01e4ebc313e64885
Yuko Zheng <1@1234.sh>
2018-12-28 16:15:41 +0000

    Merged PR 241: Support polling newdex & whaleex price

---
+++ Andoromeda.Kyubey.Timers/Jobs/NewDexPriceJob.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Pomelo.AspNetCore.TimedJob;
+using Andoromeda.Kyubey.Models;
+
+namespace Andoromeda.Kyubey.Timers.Jobs
+{
+    public class NewDexPriceJob
+    {
+        private class NewDexPrice
+        { 
+            public NewDexPriceSymbolInfo symbolInfo { get; set; }
+        }
+
+        private class NewDexPriceSymbolInfo
+        {
+            public double bidPrice { get; set; }
+
+            public double askPrice { get; set; }
+        }
+
+        [Invoke(Begin = "2018-06-01", Interval = 1000 * 60, SkipWhileExecuting = true)]
+        public void PullNewDexPrice(KyubeyContext db)
+        {
+            var tokens = db.Tokens
+                .Where(x => !string.IsNullOrEmpty(x.NewDexId))
+                .ToList();
+
+            foreach(var x in tokens)
+            {
+                try
+                {
+                    var ret = GetNewDexPriceAsync(x.NewDexId).Result;
+                    x.NewDexAsk = ret.ask;
+                    x.NewDexBid = ret.bid;
+                    db.SaveChanges();
+                }
+                catch
+                {
+                    // TODO: Log failures
+                }
+            }
+        }
+
+        private async Task<(double ask, double bid)> GetNewDexPriceAsync(string newdexId)
+        {
+            using (var client = new HttpClient { BaseAddress = new Uri("https://newdex.io") })
+            using (var response = await client.PostAsync("/api/symbol/getSymbolInfo", new FormUrlEncodedContent(new Dictionary<string, string> {
+                { "symbol", newdexId }
+            })))
+            {
+                var result = await response.Content.ReadAsAsync<NewDexPrice>();
+                return (result.symbolInfo.askPrice, result.symbolInfo.bidPrice);
+            }
+        }
+    }
+}
---
+++ Andoromeda.Kyubey.Timers/Jobs/WhaleExPriceJob.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Andoromeda.Kyubey.Models;
+using Pomelo.AspNetCore.TimedJob;
+
+namespace Andoromeda.Kyubey.Timers.Jobs
+{
+    public class WhaleExPriceJob
+    {
+        private class WhaleExPriceItem
+        { 
+            public string baseCurrency { get; set; }
+
+            public double lastPrice { get; set; }
+        }
+
+        [Invoke(Begin = "2018-06-01", Interval = 1000 * 60, SkipWhileExecuting = true)]
+        public void PullNewDexPrice(KyubeyContext db)
+        {
+            var tokens = db.Tokens
+                .Where(x => !string.IsNullOrEmpty(x.NewDexId))
+                .ToList();
+
+            var results = GetWhaleExPriceAsync().Result;
+
+            foreach (var x in tokens)
+            {
+                if (!results.Any(y => y.baseCurrency == x.Id))
+                {
+                    continue;
+                }
+
+                x.WhaleExPrice = results.First(y => y.baseCurrency == x.Id).lastPrice;
+            }
+        }
+
+        private async Task<IEnumerable<WhaleExPriceItem>> GetWhaleExPriceAsync()
+        {
+            using (var client = new HttpClient { BaseAddress = new Uri("https://www.whaleex.com") })
+            using (var response = await client.GetAsync("/BUSINESS/api/public/symbol"))
+            {
+                return await response.Content.ReadAsAsync<IEnumerable<WhaleExPriceItem>>();
+            }
+        }
+    }
+}
